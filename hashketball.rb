f224b46eccc49028a336f0b5e36d3f799c921787
Angela Lo <angelawenlo@gmail.com>
2019-05-20 16:21:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,2 +1,12 @@
 # Write your code here!
 require "pry"
+def game_hash
+  game_hash = {
+    :home => {
+      :team_name => {},
+      :colors => {},
+      :players => {}
+    },
+
+    :away
+  }
