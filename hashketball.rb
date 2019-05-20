3011c1b7da3997d15606f3a17ee7a97ee7a16e01
Angela Lo <angelawenlo@gmail.com>
2019-05-20 16:59:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -148,6 +148,7 @@ def team_colors(team)
   game_hash.each do |location, team_info|
     team_info.each do |team_colors, detail|
       if detail == team
+        return detail[:colors]
 
   binding.pry
       end
