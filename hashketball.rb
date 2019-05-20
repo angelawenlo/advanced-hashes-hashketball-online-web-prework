c1605b1266d2e18c8e799be77d16d7b8a491230b
Angela Lo <angelawenlo@gmail.com>
2019-05-20 17:23:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -157,9 +157,7 @@ end
 def team_names
   game_hash.each do |location, team_info|
   team_info.each do|team, team_name|
-      binding.pry
     return team_info[:team_name]
-
   end
-  end
+end.flatten
 end
